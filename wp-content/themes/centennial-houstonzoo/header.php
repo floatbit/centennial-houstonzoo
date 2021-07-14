bd7b6e6a1688f7449b3bcf8dd7c8885a9ceb14ab
Chris Manlapid <chris@floatbit.com>
2021-07-14 11:59:58 -0400

    added webfonts

--- wp-content/themes/centennial-houstonzoo/header.php
+++ wp-content/themes/centennial-houstonzoo/header.php
@@ -4,6 +4,8 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href='https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/ca4afe9e3d.js" crossorigin="anonymous"></script>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
