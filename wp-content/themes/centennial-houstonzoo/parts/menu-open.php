d50dd7e4af58a08ed4152c7f84dc080ad6af37f6
Juned <juned.tan@gmail.com>
2021-09-15 23:15:30 +0700

    header fix

--- wp-content/themes/centennial-houstonzoo/parts/menu-open.php
+++ wp-content/themes/centennial-houstonzoo/parts/menu-open.php
@@ -12,7 +12,7 @@
 			<div class="grid-x grid-margin-x align-middle">
 				<div class="cell medium-3 show-for-medium">
 				</div>
-				<div class="cell medium-8 small-10 logo-content flex-container align-middle align-center">
+				<div class="cell medium-8 small-10 logo-content flex-container">
 					<a href="/" class="logo-image-container">
 						<img class="logo-white" src="<?php print TEMPLATE_IMAGE_PATH."/logo-main.svg"; ?>">
 						<img class="compact-logo-white" src="<?php print TEMPLATE_IMAGE_PATH."/logo-main-compact.svg"; ?>">
